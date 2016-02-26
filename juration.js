091fc598ba2225bb80d0872b7bfa249dfab48514
albertony <albertony@gmail.com>
2016-02-26 08:38:25 +0100

    Norwegian version
    It is understanding both Nynorsk and Bokmål. When performing stringify it picks Bokmål.

--- juration.js
+++ juration.js
@@ -1,83 +1,84 @@
 /*
- * juration - a natural language duration parser
+ * juration-no - a natural language duration parser
  * https://github.com/domchristie/juration
  *
- * Copyright 2011, Dom Christie
+ * Copyright 2016, Albertony
  * Licenced under the MIT licence
  *
+ * Based on work by Dom Christie
  */
 
 (function() {
 
   var UNITS = {
     seconds: {
-      patterns: ['second', 'sec', 's'],
+      patterns: ['sekund(er)?', 'sek?', 's'],
       value: 1,
       formats: {
         'chrono': '',
         'micro':  's',
-        'short':  'sec',
-        'long':   'second'
+        'short':  'sek',
+        'long':   'sekund'
       }
     },
     minutes: {
-      patterns: ['minute', 'min', 'm(?!s)'],
+      patterns: ['minutt(er)?', 'mi?n', 'n'], // NB: m is treated as month, minutes is 'n'!
       value: 60,
       formats: {
         'chrono': ':',
-        'micro':  'm',
+        'micro':  'n', // NB: When stringifying we use 'n' instead of 'm' to avoid ambiguity with month.
         'short':  'min',
-        'long':   'minute'
+        'long':   'minutt'
       }
     },
     hours: {
-      patterns: ['hour', 'hr', 'h'],
+      patterns: ['timer?', 'tmr?', 't'],
       value: 3600,
       formats: {
         'chrono': ':',
-        'micro':  'h',
-        'short':  'hr',
-        'long':   'hour'
+        'micro':  't',
+        'short':  'tm',
+        'long':   'time'
       }
     },
     days: {
-      patterns: ['day', 'dy', 'd'],
+      patterns: ['dag(er)?', 'dag', 'dgr?', 'd'],
       value: 86400,
       formats: {
         'chrono': ':',
         'micro':  'd',
-        'short':  'day',
-        'long':   'day'
+        'short':  'dg',
+        'long':   'dag'
       }
     },
     weeks: {
-      patterns: ['week', 'wk', 'w'],
+      patterns: ['uker?', 'veker?', 'uk?', 'vk?'],
       value: 604800,
       formats: {
         'chrono': ':',
-        'micro':  'w',
-        'short':  'wk',
-        'long':   'week'
+        'micro':  'u',
+        'short':  'uk',
+        'long':   'uke'
       }
     },
     months: {
-      patterns: ['month', 'mon', 'mo', 'mth'],
+      patterns: ['måned(er)?', 'månad(er)?', 'mnd?', 'm'],
       value: 2628000,
       formats: {
         'chrono': ':',
         'micro':  'm',
-        'short':  'mth',
-        'long':   'month'
+        'short':  'mnd',
+        'long':   'måned'
       }
     },
     years: {
-      patterns: ['year', 'yr', 'y'],
+      patterns: ['år', 'a'], // TODO: Wanted 'år?' so that one could use single-character 'å', but because the boundary character class (\b) in JavaScript regex does not handle non-ASCII characters this does not work! So the single character (micro) symbol is therefore 'a' (for anno?) instead.
       value: 31536000,
       formats: {
         'chrono': ':',
-        'micro':  'y',
-        'short':  'yr',
-        'long':   'year'
+        'micro':  'a',
+        'short':  'år',
+        'long':   'år'
       }
     }
   };
@@ -114,7 +115,7 @@
         values[i] += unit.formats[opts.format];
       }
       else {
-        values[i] += ' ' + _pluralize(values[i], unit.formats[opts.format]);
+        values[i] += ' ' + _pluralize(values[i], opts.format, unit.formats[opts.format]);
       }
       remaining = remaining % unit.value;
     }
@@ -135,6 +136,18 @@
     // returns calculated values separated by spaces
     for(var unit in UNITS) {
       for(var i = 0, mLen = UNITS[unit].patterns.length; i < mLen; i++) {
+        // TODO: The word boundary \b character class does not handle non-ascii characters like norwegian æøå correctly.
+        //   - Tried to rewrite the \b into an equivalent expression using lookbehind:
+        //        The boundary "\b" can be replaced by "(?<=\W)(?=\w)|(?<=\w)(?=\W)". The \w class represents
+        //        word characters, which in ascii is [A-Za-z0-9_], and \W is just the negated [^A-Za-z0-9_].
+        //        The idea was to replace \w to include the norwegian characters: [A-Za-z0-9_æøå], and \W negation
+        //        of that. For example like this:
+        //           var wordchars_no = 'A-Za-z0-9_æøå'
+        //           var boundary_no = '(?<=[^'+wordchars_no+'])(?=['+wordchars_no+'])|(?<=['+wordchars_no+'])(?=[^'+wordchars_no+'])'
+        //        The problem is that JavaScript does not support the positive lookbehind "?<=" assertion either, so
+        //        we are back at square one...
+        //        Until this is solved: The problematic cases are when the unit patterns are ending on æøå, so for instance
+        //        for year we cannot have 'å' as a pattern but 'år' works!
         var regex = new RegExp("((?:\\d+\\.\\d+)|\\d+)\\s?(" + UNITS[unit].patterns[i] + "s?(?=\\s|\\d|\\b))", 'gi');
         string = string.replace(regex, function(str, p1, p2) {
           return " " + (p1 * UNITS[unit].value).toString() + " ";
@@ -145,7 +158,7 @@
     var sum = 0,
         numbers = string
                     .replace(/(?!\.)\W+/g, ' ')                       // replaces non-word chars (excluding '.') with whitespace
-                    .replace(/^\s+|\s+$|(?:and|plus|with)\s?/g, '')   // trim L/R whitespace, replace known join words with ''
+                    .replace(/^\s+|\s+$|(?:og|pluss|med)\s?/g, '')   // trim L/R whitespace, replace known join words with ''
                     .split(' ');
     
     for(var j = 0, nLen = numbers.length; j < nLen; j++) {
@@ -175,8 +188,16 @@
       return s;
   };
   
-  var _pluralize = function(count, singular) {
-    return count == 1 ? singular : singular + "s";
+  var _pluralize = function(count, format, singular) {
+    if (count == 1 || singular == "år") {
+        return singular;
+    } else {
+        var plural = singular;
+        if (format == 'long' && singular[singular.length-1] != "e")
+            plural += "e";
+        plural += "r";
+        return plural;
+    }
   };
   
   var _isNumeric = function(n) {
